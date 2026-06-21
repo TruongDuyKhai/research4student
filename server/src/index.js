2ff6186ff807682dab9d1490167b8ecbab72e2a5
TruongDuyKhai <truongduykhai1012@gmail.com>
2026-06-21 14:22:02 +0700

    .

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env') });
 const app = require('./app');
 const { client } = require('./services/discordClient');
 const { startRefresher } = require('./services/cdnRefresher');
